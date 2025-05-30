af95f8dc9aa9e9428a90325bad2ee0d3f73837b5
Elana-May <r1039074@ucll.be>
2025-05-30 14:13:38 +0200

    Toegevoegd: index.js voor app startup

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-"console.log('Test update');" 
-"console.log('Test update');" 
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hallo, je CI/CD pipeline werkt!");
+});
+
+app.listen(port, () => {
+  console.log(`Server draait op http://localhost:${port}`);
+});
